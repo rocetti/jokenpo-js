a017824b47b61cdc385f336354fdb3d20c8df8be
Eredah <jprocetti@gmail.com>
2022-02-22 20:00:38 -0300

    Edited jokenpo.js; finished base game

--- jokenpo.js
+++ jokenpo.js
@@ -8,6 +8,9 @@ I Kept the pseudocode here since this game is an exercise from the odin project 
 //Create a variable to store PC choice
 //Game start message
 //Ask for input
+//Store it on the variable
+//Lowercase all characters
+//Check if its a valid input: r,p,s,rock,paper or scissors
 //True> proceed; False> ask again
 //Random roll 0~2
 //if 0 = Rock; 1 = Paper; 2 = Scissors; Store result in variable
@@ -34,104 +37,136 @@ function main(){
     let playerInput;
     let computerChoice;
     alert("This game is played through console on your browser DevTools!");
-    console.log("JOKENPO!");
-    console.log("You against the machine in this game for your soul!");
-    console.log("Win 3 games and you're free.");
-    displayScore(playerScore,computerScore);
+    //Game Loop
+    do {
+        //Start Message
+        console.log("JOKENPO!");
+        console.log("You against the machine in this game for your soul!");
+        console.log("Win 3 games and you're free.");
+        displayScore(playerScore,computerScore);
+        //Loop through rounds
+        do {
+            playerInput = askValidInput("choice");            
+            computerChoice = randomChoice();
 
-    playerInput = askValidInput("choice");
-    //console.log("Debug: variable playerInput value; " + playerInput)
-    
-    computerChoice = randomChoice();
-    console.log("Debug: variable computerChoice value; " + computerChoice)
-    let winner = compareChoices(playerInput,computerChoice);
-    switch (winner) {
-        case "Player":
-            ++playerScore;
+            let winner = compareChoices(playerInput,computerChoice);
+            switch (winner) {
+                case "Player":
+                    ++playerScore;
+                    break;
+                case "Machine":
+                    ++computerScore;
+                    break;
+                default:
+                    break;
+            }
+            
+            console.log("And the winner for this round was...");
+            console.log(winner);
+            displayScore(playerScore,computerScore);
+            if (playerScore >=3 || computerScore >=3) {
+                break;
+            }            
+        } while (true);
+
+        if (playerScore >= 3 || computerScore >= 3) {
+            let champion = playerScore > computerScore?  "Player" : "Machine"
+            finishGame(champion);
+            playerInput = askValidInput("replay");
+        }
+        if (playerInput === "y") {
+            playerScore = 0;
+            computerScore = 0;
+            console.clear();
+        } else {
+            console.log("Ok! Bye");
             break;
-        case "Machine":
-            ++computerScore;
-            break;
-        default:
-            break;
-    }
-    
-    console.log("And the winner for this round was...");
-    console.log(winner);
-    displayScore(playerScore,computerScore);
-    if (playerScore >= 3 || computerScore >= 3) {
-        let champion = playerScore > computerScore?  "Player" : "Machine"
-        finishGame(champion);
-        playerInput = askValidInput("replay");
-    }
-    if (playerInput) {
-        console.clear();
-    } else {
-        console.log("Ok! Bye");
-        break;
-    }
+        }
+        
+    } while (true);
 }
 
 function displayScore(playerScore,computerScore) {
     console.log("You won " + playerScore + " rounds");
     console.log("Computer won " + computerScore + " rounds");
 }
-
+//Ask player for input until its an valid text for the game state
 function askValidInput(state){
     let inputIsValid = false;
     let newInput;
-    do {
-        //Store it on the variable
-        newInput = window.prompt("Enter your weapon of choice: Rock, Paper or Scissors!");
-        //Lowercase all characters
-        newInput = newInput.toLowerCase();
-        //console.log("Debug: variable newInput value; " + newInput)
-        //Check if its a valid input: r,p,s,rock,paper or scissors
-        switch (newInput) {
-            case "r":                
-                inputIsValid = true;
-                console.log("You chose Rock.");
-                break;
-            case "p":
-                inputIsValid = true;
-                console.log("You chose Paper.");
-                break;
-            case "s":
-                inputIsValid = true;
-                console.log("You chose Scissors.");
-                break;
-            case "rock":                
-                inputIsValid = true;
-                console.log("You chose Rock.");
-                break;
-            case "paper":
-                inputIsValid = true;
-                console.log("You chose Paper.");
-                break;
-            case "scissors":
-                inputIsValid = true;
-                console.log("You chose Scissors.");
-                break;
-            default:
-                inputIsValid = false;
-                console.log("You inserted invalid text. Please enter a valid option.");
-                break;
-        }   
+    do {        
+        
+        if (state === "choice") {
+            newInput = window.prompt("Enter your weapon of choice: Rock, Paper or Scissors!");
+    
+            newInput = newInput.toLowerCase();
+            switch (newInput) {
+                case "r":                
+                    inputIsValid = true;
+                    console.log("You chose Rock.");
+                    break;
+                case "p":
+                    inputIsValid = true;
+                    console.log("You chose Paper.");
+                    break;
+                case "s":
+                    inputIsValid = true;
+                    console.log("You chose Scissors.");
+                    break;
+                case "rock":                
+                    inputIsValid = true;
+                    console.log("You chose Rock.");
+                    break;
+                case "paper":
+                    inputIsValid = true;
+                    console.log("You chose Paper.");
+                    break;
+                case "scissors":
+                    inputIsValid = true;
+                    console.log("You chose Scissors.");
+                    break;
+                default:
+                    inputIsValid = false;
+                    console.log("You inserted invalid text. Please enter a valid option.");
+                    break;
+            }                        
+        } else if(state === "replay") {
+            newInput = window.prompt("Do you wanna play again?");
+    
+            newInput = newInput.toLowerCase();
+            newInput = newInput.charAt(0)
+            switch (newInput) {
+                case "y":
+                    inputIsValid = true;
+                    break;  
+                case "n":
+                    inputIsValid = true;
+                    break;           
+                default:
+                    inputIsValid = false;
+                    console.log("You inserted invalid text. Please enter a valid option.");
+                    break;            
+            }
+        } 
     } while (!inputIsValid);
+    inputIsValid = false;
     return newInput;
 }
 
 function randomChoice() {
-    let randi = (Math.random() * 3);
+    let randi = Math.floor((Math.random() * 2)+0.5);
     switch (randi) {
         case 0:
+            console.log("Machine chose Rock.");
             return "rock";
         case 1:
+            console.log("Machine chose Paper.");
             return "paper";
         case 2:
+            console.log("Machine chose Scissors.");
             return "scissors";
         default:
-            console.log("ERROR: Computer couldn't choose.");
+            console.log("ERROR: Machine couldn't choose.");
             break;
     }
 }
